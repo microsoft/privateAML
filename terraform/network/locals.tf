35988a9d6bda43c9285a8eed5620eb59e72e9b23
Denis Cepun <denis.cepun@microsoft.com>
2022-03-07 13:21:37 +0200

    updated vars

--- terraform/network/locals.tf
+++ terraform/network/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  core_services_vnet_subnets            = cidrsubnets(var.vnet_address_space,4, 3, 4, 4)
+  core_services_vnet_subnets            = cidrsubnets(var.vnet_address_space,5, 3, 4, 3)
   firewall_subnet_address_space         = local.core_services_vnet_subnets[1]
   bastion_subnet_address_prefix         = local.core_services_vnet_subnets[2]
   shared_services_subnet_address_prefix = local.core_services_vnet_subnets[3]
